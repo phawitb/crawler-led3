c45804e5f88282be27427515fa4d542a020889f0
phawitb <96680592+phawitb@users.noreply.github.com>
2023-04-22 17:00:44 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py nonthaburi
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py nonthaburi
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 2_find_gps.py nonthaburi
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 3_combile_data.py nonthaburi
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 4_sent_to_DB.py nonthaburi
+/home/phawit/anaconda3/envs/crawler-led3/bin/python 1_crawler_led.py nakhonpathom
+/home/phawit/anaconda3/envs/crawler-led3/bin/python 2_find_gps.py nakhonpathom
+/home/phawit/anaconda3/envs/crawler-led3/bin/python 3_combile_data.py nakhonpathom
 
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py nakhonpathom
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py nakhonpathom
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 2_find_gps.py nakhonpathom
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 3_combile_data.py nakhonpathom
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 4_sent_to_DB.py nakhonpathom
 
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py bangkok
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py bangkok
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 3_combile_data.py bangkok
-/home/phawit/anaconda3/envs/crawler-led2/bin/python 4_sent_to_DB.py bangkok
+
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py nonthaburi
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py nonthaburi
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 2_find_gps.py nonthaburi
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 3_combile_data.py nonthaburi
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 4_sent_to_DB.py nonthaburi
+
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py nakhonpathom
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py nakhonpathom
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 2_find_gps.py nakhonpathom
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 3_combile_data.py nakhonpathom
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 4_sent_to_DB.py nakhonpathom
+
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py bangkok
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 1_crawler_led.py bangkok
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 3_combile_data.py bangkok
+#/home/phawit/anaconda3/envs/crawler-led2/bin/python 4_sent_to_DB.py bangkok
 
 # /home/phawit/anaconda3/envs/crawler-led2/bin/python 2_find_gps.py bangkok
 # /home/phawit/anaconda3/envs/crawler-led2/bin/python 3_combile_data.py bangkok
